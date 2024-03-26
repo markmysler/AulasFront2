a321c9c9bafc5a06e558d52f55e1b8023ae28749
Mark Mysler <myslermark@gmail.com>
2024-03-26 17:40:33 -0300

    fix sw.js

--- vue.config.js
+++ vue.config.js
@@ -24,13 +24,5 @@ module.exports = {
 			start_url: ".",
 			display: "fullscreen",
 		},
-
-		// configure the workbox plugin
-		workboxPluginMode: "InjectManifest",
-		workboxOptions: {
-			// swSrc is required in InjectManifest mode.
-			swSrc: "dev/sw.js",
-			// ...other Workbox options...
-		},
 	},
 };
